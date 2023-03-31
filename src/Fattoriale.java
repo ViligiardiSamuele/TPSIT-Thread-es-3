889252dfaaea98a82163910729407d0a4c41439d
ViligiardiSamuele <viligiardi.samuele@itismeucci.com>
2023-03-31 16:15:01 +0200

    v0.1

---
+++ src/Fattoriale.java
@@ -0,0 +1,25 @@
+public class Fattoriale extends Thread{
+    private int val;
+
+    public Fattoriale(String name, int val) {
+        super(name);
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    @Override
+    public void run() {
+        int out = 1;
+        for (int i = val; i > 0; i--)
+            out *= i;
+        val = out;
+        try {
+            this.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner s = new Scanner(System.in);
+        int n,k;
+        System.out.print("Inserisci k: ");
+        do {
+            k = s.nextInt();
+            s.reset();
+        } while (k < 0);
+        System.out.print("Inserisci n (rispettando n > k): ");
+        do {
+            n = s.nextInt();
+            s.reset();
+        } while (n < 0 || n <= k);
+
+        Fattoriale fn = new Fattoriale("F di n",n);
+        Fattoriale fk = new Fattoriale("F di k",k);
+        Fattoriale fnSubk = new Fattoriale("F di n-k",(n - k));
+        fn.start();
+        fk.start();
+        fnSubk.start();
+        System.out.println("combinazioni("+n+","+k+"): " + fn.getVal() / (fk.getVal())* fnSubk.getVal());
     }
 }
